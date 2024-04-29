7d0bd1bd2a549b5502e0d3b4f8acbc709f9e04be
Antoine Dupont <tony_dupont@yahoo.com>
2024-04-29 13:38:39 +0200

    display_table

--- functions.py
+++ functions.py
@@ -10,3 +10,26 @@ def read_table(filename):
             table_data.append(row_data)
 
     return table_data
+
+def display_table(table_data):
+    max_rows = min(len(table_data) - 1, 20)  # Exclude the last row (Orders)
+    max_columns = min(len(table_data[0]), 12)
+
+    # Display column headers
+    print('          ', end='')  
+    for col in range(1, max_columns + 1):
+        print(f'C{col:<4}', end='')  # Updated column headers
+    print()
+
+    # Display table data
+    for i in range(max_rows):
+        print('    ', end='')  
+        print(f'P{i + 1:2}', end='')
+        for j in range(max_columns):
+            print(f'{table_data[i][j]:5}', end='')
+        print()
+
+    # Display capacities
+    print('Orders:', end='')
+    for j in range(max_columns):
+        print(f'{table_data[-1][j]:5}', end='')
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import functions as f
 
+def main():
+    while(True):
+        nb_table = input("\n\nWhat table do you want to test ?\n")
+        filename = 'table'+nb_table+'.txt'
+        #filename = 'table1.txt'  
+        table_data = f.read_table(filename)
+        f.display_table(table_data)
+        
 if __name__ == "__main__":
-    filename = 'table1.txt'  
-    table_data = f.read_table(filename)
-    print(table_data)
+    main()
