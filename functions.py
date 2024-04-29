046c43b38c8bb4d2249226248a00265983b7d66c
Antoine Dupont <tony_dupont@yahoo.com>
2024-04-29 13:20:14 +0200

    modifications

---
+++ functions.py
@@ -0,0 +1,12 @@
+
+def read_table(filename):
+    table_data = []
+
+    with open(filename, 'r') as file:
+        rows, columns = map(int, file.readline().split())
+
+        for _ in range(rows):
+            row_data = list(map(int, file.readline().split()))
+            table_data.append(row_data)
+
+    return table_data
---
+++ main.py
@@ -0,0 +1,6 @@
+import functions as f
+
+if __name__ == "__main__":
+    filename = 'table1.txt'  
+    table_data = f.read_table(filename)
+    print(table_data)
