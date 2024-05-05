96886ab85b9399226d0e97e9af51d53269a40a2a
Antoine Dupont <tony_dupont@yahoo.com>
2024-05-05 11:12:26 +0200

    alex modifs

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import sys
+import copy
 
 def read_table(filename):
     table_data = []
@@ -81,271 +81,195 @@ def north_west_corner_method(table_data):
     # Return the updated matrix
     return new_matrice
 
-def max_column(table_data):
-    max_columns = min(len(table_data[0]) - 1, 13)  # Exclude the last column (Provision)
-    max_colum = []
-    for i in range(max_columns):
-        max_value = float('-inf')  
-        for row in table_data[:-1]:
-            if row[i] > max_value:
-                max_value = row[i]
-        max_colum.append(max_value)
-    return max_colum
+def remove_flagged_items(data):
+    data_copy = copy.deepcopy(data)
+    for ind in range(len(data)):
+        if data[ind] == -1:
+            data_copy.remove(-1)
+    return data_copy
 
-def mx_row(table_data):
-    max_rows = min(len(table_data) - 1, 21)  # Exclude the last row (Orders)
-    max_row = []
-    for i in range(max_rows):
-        max_value = float('-inf')  
-        for column in table_data[i][:-1]:
-            if column > max_value:
-                max_value = column
-        max_row.append(max_value)
-    return max_row
+def row_diff(line):
+    if ((len(line)-1)>=1):
+        line_copy = copy.deepcopy(line)
+        line_copy.pop(len(line)-1) # Remove the "order" info
+        line_copy = remove_flagged_items(line_copy)
+        line_copy.sort()
 
-def mini_col(table_data):
-    max_columns = min(len(table_data[0]) - 1, 13)  # Exclude the last column (Provision)
-    min_colum = []
-    for i in range(max_columns):
-        min_value = float('inf')  
-        for row in table_data[:-1]:
-            if row[i] < min_value:
-                min_value = row[i]
-        min_colum.append(min_value)
-    return min_colum
+        if len(line_copy) == 0:
+            return -1
+        if len(line_copy) == 1:
+            return line_copy[0]
+        val1 = line_copy[0]
+        val2 = line_copy[1]
+        return abs(val1-val2)
+    return line[0]
 
-def mini_row(table_data):
-    max_rows = min(len(table_data) - 1, 21)  # Exclude the last row (Orders)
-    min_row = []
-    for i in range(max_rows):
-        min_value = float('inf')  
-        for column in table_data[i][:-1]:
-            if column < min_value:
-                min_value = column
-        min_row.append(min_value)
-    return min_row
+def col_diff(line):
+    if ((len(line)-1)>=1):
+        line_copy = copy.deepcopy(line)
+        line_copy = remove_flagged_items(line_copy)
+        line_copy.sort()
+        if len(line_copy) == 0:
+            return -1
+        if len(line_copy) == 1:
+            return line_copy[0]        
+        val1 = line_copy[0]
+        val2 = line_copy[1]
+        return abs(val1-val2)
+    return line[0]
 
-def max_difference(table_data): #return the position of the maximum difference of penalty 
-    max_rows = min(len(table_data) - 1, 21)  # Exclude the last row (Orders)
-    max_columns = min(len(table_data[0]) - 1, 13)  # Exclude the last column (Provision)
-    difference_colum = []
-    difference_row = []
-    
-    
-    max_colum = max_column(table_data)
-    max_row = mx_row(table_data)
-    min_colum = mini_col(table_data)
-    min_row = mini_row(table_data)
+def create_alloc_table(table_data, max_rows, max_cols):
+    alloc_table = [[0 for x in range(max_cols+1)] for y in range(max_rows+1)]
 
-    # Calculate the differences between the maximum and minimum values in each column
-    difference_colum = [max_colum[i] - min_colum[i] for i in range(max_columns)]
-    
-    # Calculate the differences between the maximum and minimum values in each row
-    difference_row = [max_row[i] - min_row[i] for i in range(max_rows)]
+    for ind in range (max_cols):
+        alloc_table[max_rows][ind] = table_data[max_rows][ind]
 
-    # Find the maximum value from both the difference columns and difference rows
-    max_diff_col = max(difference_colum)
-    max_diff_row = max(difference_row)
-    max_diff = max(max_diff_col, max_diff_row)
+    for ind in range (max_rows):
+        alloc_table[ind][max_cols] = table_data[ind][max_cols]
 
-    # Determine if the maximum difference is from a column or a row
-    if max_diff_col > max_diff_row:
-        max_diff_type = 'column'
-        max_diff_index = difference_colum.index(max_diff_col)
-    else:
-        max_diff_type = 'row'
-        max_diff_index = difference_row.index(max_diff_row)
-    
-    return max_diff_type, max_diff_index, max_diff
+    return alloc_table
 
-def find_cell(max_diff_type, max_diff_index, new_matrice):
-    if max_diff_type == 'row':
-        # Find the cell with the minimum value in the chosen row
-        min_value = min(new_matrice[max_diff_index][:-1])
-        min_index = new_matrice[max_diff_index][:-1].index(min_value)
+def display_alloc_table(alloc_table):
+    max_rows = len(alloc_table)
+    max_columns = len(alloc_table[0])
 
-        # If there are multiple cells with the same minimum value,
-        # choose the one that has not been previously allocated
-        for i in range(min_index + 1, len(new_matrice[max_diff_index][:-1])):
-            if new_matrice[max_diff_index][i] == min_value and new_matrice[max_diff_index][i] == 0:
-                min_index = i
-                break
-        for i in range(min_index - 1, -1, -1):
-            if new_matrice[max_diff_index][i] == min_value and new_matrice[max_diff_index][i] == 0:
-                min_index = i
-                break
+    # Display column headers
+    print('\n\n          ', end='')  
+    for col in range(1, max_columns):
+        print(f'C{col:<4}', end='')
+    print('Supply')
 
-        return max_diff_index, min_index
-    else:
-        # Find the cell with the minimum value in the chosen column
-        min_value = min(row[max_diff_index] for row in new_matrice[:-1])
-        for i, row in enumerate(new_matrice[:-1]):
-            if row[max_diff_index] == min_value:
-                # If there are multiple cells with the same minimum value,
-                # choose the one that has not been previously allocated
-                for j in range(i + 1, len(new_matrice[:-1])):
-                    if new_matrice[j][max_diff_index] == min_value and new_matrice[j][max_diff_index] == 0:
-                        i = j
-                        break
-                for j in range(i - 1, -1, -1):
-                    if new_matrice[j][max_diff_index] == min_value and new_matrice[j][max_diff_index] == 0:
-                        i = j
-                        break
-                return i, max_diff_index
-
-            
-def balas_hammer_method(table_data):
-    max_rows = min(len(table_data) - 1, 21)  # Exclude the last row (Orders)
-    max_columns = min(len(table_data[0]) - 1, 13)  # Exclude the last column (Provision)
-
-    # Initialize the indices for the northwest corner of the matrix
-    row_index = 0
-    col_index = 0
-
-    new_matrice = table_data
-
-    # Change each value to 0 except the last line and last column
-    for i in range(max_rows):
+    # Display table data
+    for i in range(max_rows - 1):  # Exclude the last row (Orders)
+        print('    ', end='')  
+        print(f'P{i + 1:2}', end='')
         for j in range(max_columns):
-            new_matrice[i][j] = 0
+            print(f'{alloc_table[i][j]:5}', end='')
+        print()
 
-    # Iterate until we reach the last row or column
-    while row_index < max_rows and col_index < max_columns:
-        # Find the row or column with the maximum penalty
-        max_diff_type, max_diff_index, max_diff = max_difference(new_matrice)
+    # Display capacities
+    print('Orders:', end='')
+    for j in range(max_columns - 1):  # Exclude the last capacity
+        print(f'{alloc_table[-1][j]:5}', end='')
+    print()  # Add a new line for formatting
 
-        # Find the cell with the minimum value in the chosen row or column
-        row_index, col_index = find_cell(max_diff_type, max_diff_index, new_matrice)
+def get_min_not_flagged(line):
+    line_copy = remove_flagged_items(line)
+    return min(line_copy)
 
-        # Allocate the maximum possible value to the current cell
-        min_value = min(new_matrice[row_index][-1], new_matrice[-1][col_index])
-        new_matrice[row_index][col_index] = min_value
+def get_column_data(table_data, col, max_rows):
+    col_data = [0]*(max_rows)
+    for ind in range(max_rows):
+        col_data[ind]=table_data[ind][col]
+    return col_data
 
-        # Update supply and demand
-        new_matrice[row_index][-1] -= min_value
-        new_matrice[-1][col_index] -= min_value
-
-        # Move to the next row or column if supply or demand becomes zero
-        if new_matrice[row_index][-1] == 0:
-            row_index += 1
-        else:
-            col_index += 1
-
-        # Display the updated table
-        display_table(new_matrice)
-
-    # Return the updated matrix
-    return new_matrice
-
-
-def get_potentials(table_data):
-    max_rows = len(table_data) - 1
-    max_columns = len(table_data[0]) - 1
-
-    row_potentials = [0] * max_rows
-    col_potentials = [0] * max_columns
-
-    for i in range(max_rows):
-        allocated_cells = [j for j in range(max_columns) if table_data[i][j] > 0]
-        if len(allocated_cells) > 0:
-            row_potentials[i] = min(table_data[i][j] - col_potentials[j] for j in allocated_cells)
-
-    for j in range(max_columns):
-        allocated_cells = [i for i in range(max_rows) if table_data[i][j] > 0]
-        if len(allocated_cells) > 0:
-            col_potentials[j] = min(table_data[i][j] - row_potentials[i] for i in allocated_cells)
-
-    return row_potentials, col_potentials
-
-def is_optimal(table_data, row_potentials, col_potentials):
-    max_rows = len(table_data) - 1
-    max_columns = len(table_data[0]) - 1
-
-    for i in range(max_rows):
-        for j in range(max_columns):
-            if table_data[i][j] - row_potentials[i] - col_potentials[j] > 0:
-                return False
-
+def is_column_flagged(col_data):
+    for ind in range(len(col_data)):
+        if col_data[ind] != -1:
+            return False
     return True
 
-def get_next_edge(table_data, row_potentials, col_potentials):
-    max_rows = len(table_data) - 1
-    max_columns = len(table_data[0]) - 1
+def vogel(table_data):
 
-    min_penalty = sys.maxsize
-    selected_row = -1
-    selected_col = -1
+    initial_table_data = copy.deepcopy(table_data)
 
-    for i in range(max_rows):
-        for j in range(max_columns):
-            if table_data[i][j] > 0:
-                penalty = table_data[i][j] - row_potentials[i] - col_potentials[j]
-                if penalty < min_penalty:
-                    min_penalty = penalty
-                    selected_row = i
-                    selected_col = j
+    max_rows = len(table_data)-1
+    max_cols = len(table_data[0])-1
 
-    return selected_row, selected_col
+    alloc_table = create_alloc_table(table_data, max_rows, max_cols)
 
-def update_transport_graph(table_data, row_index, col_index):
-    max_rows = len(table_data) - 1
-    max_columns = len(table_data[0]) - 1
+    # Algorithm start
+    do_loop = True
+    while do_loop:
 
-    # Find the cycle or unconnected part of the transport graph
-    visited = [[False] * max_columns for _ in range(max_rows)]
-    stack = [(row_index, col_index)]
-    while stack:
-        i, j = stack.pop()
-        if not visited[i][j]:
-            visited[i][j] = True
-            for k in range(max_columns):
-                if table_data[i][k] > 0 and not visited[i][k]:
-                    stack.append((i, k))
-            for k in range(max_rows):
-                if table_data[k][j] > 0 and not visited[k][j]:
-                    stack.append((k, j))
+        row_diffs = [0]*max_rows
+        col_diffs = [0]*max_cols
 
-    # Modify the transport graph
-    for i in range(max_rows):
-        for j in range(max_columns):
-            if visited[i][j] and table_data[i][j] > 0:
-                table_data[i][j] -= 1
-                table_data[i][-1] -= 1
-                table_data[-1][j] -= 1
-            elif not visited[i][j] and table_data[i][j] < table_data[row_index][col_index]:
-                table_data[i][j] += 1
-                table_data[i][-1] += 1
-                table_data[-1][j] += 1
+        # Compute row differences and col differences
+        for line in range(max_rows):
+            row_diffs[line] = row_diff(table_data[line])
 
-def stepping_stone_method(table_data):
-    row_potentials, col_potentials = get_potentials(table_data)
+        # Compute col differences
+        row_ind=0
+        for col in range(max_cols):
+            
+            col_data = get_column_data(table_data, col, max_rows)
+            if is_column_flagged(col_data):
+                col_diffs[col] = -1
+                row_ind +=1
+                continue
 
-    while not is_optimal(table_data, row_potentials, col_potentials):
-        row_index, col_index = get_next_edge(table_data, row_potentials, col_potentials)
+            col_diffs[col] = col_diff(col_data)
+            row_ind +=1
 
-        # Display the transport proposal and the total transport cost
-        print("Transport proposal:")
-        display_table(table_data)
-        print(f"Total transport cost: {get_total_cost(table_data)}")
+        print ("Row-diffs => ",row_diffs)
+        print ("Col-diffs => ",col_diffs)
 
-        # Update the transport graph
-        update_transport_graph(table_data, row_index, col_index)
+        # Now identify the max between rows diff and cols diff
+        max_rows_diff = max(row_diffs)
+        max_cols_diff = max(col_diffs)
 
-        # Recalculate potentials
-        row_potentials, col_potentials = get_potentials(table_data)
+        # Stop here if everything has already been processed
+        if max_rows_diff == -1 and max_cols_diff == -1:
+            # Compute and return the total cost
+            total_cost = 0
+            for row in range(max_rows):
+                for col in range(max_cols):
+                    alloc = alloc_table[row][col]
+                    if alloc != 0:
+                        total_cost += initial_table_data[row][col]*alloc
+            print(f'Total cost: {total_cost}')
+            return total_cost
 
-    # Display the minimal transportation proposal and its cost
-    print("Minimal transportation proposal:")
-    display_table(table_data)
-    print(f"Minimal cost: {get_total_cost(table_data)}")
+        alloc_row = 0
+        alloc_col = 0
 
-def get_total_cost(table_data):
-    max_rows = len(table_data) - 1
-    max_columns = len(table_data[0]) - 1
-    total_cost = 0
+        # Compute allocation row/col
+        if(max_rows_diff > max_cols_diff):
+            row_ind = row_diffs.index(max_rows_diff)
 
-    for i in range(max_rows):
-        for j in range(max_columns):
-            total_cost += table_data[i][j] * table_data[i][-1] * table_data[-1][j]
+            # Get min cost of that row
+            min_cost = get_min_not_flagged(table_data[row_ind])
 
-    return total_cos
+            # Identify col with the min cost
+            alloc_row = row_ind
+            alloc_col = table_data[row_ind].index(min_cost)
+            
+            supply = alloc_table[row_ind][max_cols]
+            orders = alloc_table[max_rows][alloc_col]
+
+        else:
+            col_ind = col_diffs.index(max_cols_diff)
+
+            # Collect this col data
+            col_data = get_column_data(table_data, col_ind, max_rows)
+
+            # Get min cost of that col
+            min_cost = get_min_not_flagged(col_data)
+
+            # Identify row with the min cost
+            alloc_row = col_data.index(min_cost)
+            alloc_col = col_ind
+
+            supply = alloc_table[alloc_row][max_cols]
+            orders = alloc_table[max_rows][alloc_col]
+
+        # Check supply vs orders
+        # Save allocation
+        if orders <= supply:
+            alloc_table[max_rows][alloc_col] = 0 # Enough supply => is fulfilled
+            alloc_table[alloc_row][max_cols] = supply - orders # Spare supply
+            alloc_table[alloc_row][alloc_col] = orders # Orders completed
+
+            for ind in range(max_rows):
+                table_data[ind][alloc_col] = -1 # flag this entire column as it is now processed
+
+        else:
+            alloc_table[max_rows][alloc_col] = orders-supply # Not all orders fulfilled
+            alloc_table[alloc_row][max_cols] = 0 # Spare supply
+            alloc_table[alloc_row][alloc_col] += supply # Orders completed
+
+            for ind in range(max_cols):
+                table_data[alloc_row][ind] = -1 # flag this entire row as it is now processed
+
+        display_alloc_table(alloc_table)
--- main.py
+++ main.py
@@ -1,35 +1,18 @@
 import functions as f
 
 def main():
-    while True:
-        nb_table = input("Enter the name of the file containing the table data: ")
-        filename = "table"+nb_table+".txt"
+    while(True):
+        nb_table = input("\n\nWhat table do you want to test ?\n")
+        filename = 'table'+nb_table+'.txt' 
         table_data = f.read_table(filename)
-
-        print("\nInitial table:")
+        print(f"\nHere is the table {nb_table} :")
         f.display_table(table_data)
-
-        while True:
-            algorithm = input("\nChoose the algorithm for the initial basic feasible solution (N for North-West Corner Method, B for Balas-Hammer Method): ")
-
-            if algorithm.lower() == 'n':
-                initial_solution = f.north_west_corner_method(table_data)
-                break
-            elif algorithm.lower() == 'b':
-                initial_solution = f.balas_hammer_method(table_data)
-                break
-            else:
-                print("Invalid algorithm choice. Please try again.")
-
-        print("\nInitial basic feasible solution:")
-        f.display_table(initial_solution)
-
-        f.stepping_stone_method(initial_solution)
-
-        continue_program = input("\nDo you want to solve another transportation problem? (Y/N): ")
-        if continue_program.lower() != 'y':
-            print("Exiting the program...")
-            break
-
+        method = int(input("\nWhat method do you want ? (1 for North-West, 2 for Balas-Hammer) ?\n"))
+        if method == 1:
+            nw_matrice = f.north_west_corner_method(table_data)
+           
+        else:
+            nw5_matrice = f.vogel(table_data)
+           
 if __name__ == "__main__":
     main()
